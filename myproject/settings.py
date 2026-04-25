33b7f9b4a4be0a3ff949b230c81e576ea6a0d2ce
Neeraj Tiwari <neerajtiwari202082@gmail.com>
2026-04-26 02:46:06 +0530

    FINAL FIX

--- myproject/settings.py
+++ myproject/settings.py
@@ -10,14 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import os
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = ['https://realtymind.onrender.com']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
