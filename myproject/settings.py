d5973819a48782f829f9c30dd34d4554fd5d1dcd
Neeraj Tiwari <neerajtiwari202082@gmail.com>
2026-04-26 02:15:16 +0530

    fixed BASE_DIR error

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 from pathlib import Path
 ALLOWED_HOSTS = ['*']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Quick-start development settings - unsuitable for production
