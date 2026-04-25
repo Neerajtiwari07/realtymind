ccfa7769d253481c31e0bc0fa1b0401718b3e08f
Neeraj Tiwari <neerajtiwari202082@gmail.com>
2026-04-26 02:20:19 +0530

    fix STATIC_ROOT order

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-
 from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 ALLOWED_HOSTS = ['*']
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
